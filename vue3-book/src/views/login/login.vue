26aff0a5af33ef1f6690238e35cf8343f8357ec2
阿豪 <2670321326@qq.com>
2023-07-04 16:14:29 +0800

    前端1

--- vue3-book/src/views/login/login.vue
+++ vue3-book/src/views/login/login.vue
@@ -1,21 +1,22 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+
 <template>
-    <div class="login">
-        <LoginChild></LoginChild>
-    </div>
+  <div class="login">
+    <LoginChild></LoginChild>
+  </div>
 </template>
-  
+
 <script setup lang="ts">
 import LoginChild from "./c-cpns/login-child.vue";
 </script>
-  
+
 <style scoped lang="less">
 .login {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 }
 </style>
-  
